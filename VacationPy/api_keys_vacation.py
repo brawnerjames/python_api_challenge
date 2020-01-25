1f131236e835a87665dfbadbf9ebb554b3016719
Jimmy Brawner <56943140+brawnerjames@users.noreply.github.com>
2020-01-25 18:01:56 -0500

    Add files via upload

---
+++ VacationPy/api_keys_vacation.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "597a8910dab83b6fa0e2f3a551f2e3ae"
+
+# Google API Key
+gkey = "AIzaSyBo0SQBLtQ6-7hbkWFdHVw7bwW7APLru9c"
---
+++ WeatherPy/api_keys_weather.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "597a8910dab83b6fa0e2f3a551f2e3ae"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
